cb3291779f359a4e07e01bd42f7e67c260db9704
ahuchley7080 <98286791+ahuchley7080@users.noreply.github.com>
2022-02-23 11:59:18 -0800

    Update linear_algebra.py

--- linear_algebra.py
+++ linear_algebra.py
@@ -18,7 +18,7 @@ assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
 
 def subtract(v: Vector, w: Vector) -> Vector:
     """Subtracts corresponding elements"""
-    assert len(v) == len(w), "vectors must be the same length"
+    #assert len(v) == len(w), "vectors must be the same length"
     return [v_i - w_i for v_i, w_i in zip(v,w)]
     
 assert subtract([5, 7, 9], [4, 5, 6] == [1, 2, 3])
